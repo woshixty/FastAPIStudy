817348c02f1be6a7eaec28fd9ed6ed8fd4fcf1cd
谢庭宇 <qyyzxty@icloud.com>
2023-08-12 13:02:01 +0800

    request params

--- test/d03_request_params.py
+++ test/d03_request_params.py
@@ -1,25 +1,34 @@
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, Body, Form
 
 app = FastAPI()
 
 
-@app.get("/user/{id}")
-def user(id):
+# get param /?
+@app.get("/param1/{id}")
+def param1(id):
     return {"id": id}
 
 
-
-@app.get("/user2")
-def user2(id):
+# get param ?
+@app.get("/param2")
+def param2(id):
     return {"id": id}
 
 
 # get header data
-@app.get("/user3")
-def user3(id, token=Header(None)):
+@app.get("/tokenGet")
+def tokenGet(id, token=Header(None)):
     return {"id": id, "token": token}
 
 
 # get body data
-@app.get("login")
-def login():
+@app.post("/bodyGet")
+def bodyGet(data=Body(None)):
+    return {"data": data}
+
+
+# get form data
+@app.post("/formGet")
+def formGet(username=Form(None), passwd=Form(None)):
+    return {"data": {"username": username, "passwd": passwd}}
+
