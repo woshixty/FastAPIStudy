10fcdda8464720e15054d83acd40619ba3ba0142
351453848@qq.com <qyyzxty@icloud.com>
2023-08-12 16:26:53 +0800

    d07_tortoise_orm

--- basic/d07_tortoise_orm.py
+++ basic/d07_tortoise_orm.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
+from tortoise.contrib.fastapi import register_tortoise
+
 
 app = FastAPI()
 template = Jinja2Templates("pages")
 todos = ["write diary", "watch movie", "play game"]
+
+# 数据库绑定
+register_tortoise(app,
+                  db_url="mysql://root:123456@localhost:3306/fastapi1",
+                  modules={"models":[]},
+                  add_exception_handlers=True,
+                  generate_schemas=True)
 
 
 # response html page
